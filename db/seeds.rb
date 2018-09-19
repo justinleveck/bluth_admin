c7f5c1c949f8e61c43a31e4a3b61b826016bda63
Justin Leveck <jleveck@gmail.com>
2018-09-19 16:10:46 -0700

    Add seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+5.times do
+  Company.create(name: Faker::Company.name)
+end
+
+Company.all.each do |company|
+  5.times do
+    password = Faker::Internet.password
+
+    Customer.create(
+      company_id: company.id,
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.first_name,
+      email: Faker::Internet.email,
+      password: password,
+      password_confirmation: password
+    )
+  end
+end
+
+
