1c140b0ae7e5bf2485611e44042688ddce0a9547
Justin Leveck <jleveck@gmail.com>
2018-09-19 18:21:30 -0700

    Add feature spec for admins filtering customers by company

--- spec/features/admin_viewing_customers_spec.rb
+++ spec/features/admin_viewing_customers_spec.rb
@@ -1,12 +1,24 @@
 require 'rails_helper'
 
 feature 'Admin viewing customers' do
-	scenario 'with valid input' do
+	scenario 'with pagination' do
     company = Fabricate(:company)
     Fabricate.times(100, :customer, company: company)
 
 		visit customers_path
 
     expect(page).to have_xpath(".//tbody//tr", :count => 50)
+	end
+
+	scenario 'filtering by company' do
+    company_1 = Fabricate(:company)
+    Fabricate.times(5, :customer, company: company_1)
+
+    company_2 = Fabricate(:company)
+    Fabricate.times(10, :customer, company: company_2)
+
+    visit customers_path company_id: company_1.id
+
+    expect(page).to have_xpath(".//tbody//tr", :count => 5)
 	end
 end
