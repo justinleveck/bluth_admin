83194e433b0d3a8e2fa528f674062cc5063165d9
Justin Leveck <jleveck@gmail.com>
2018-09-19 23:13:17 -0700

    Fix tab alignment

--- spec/features/admin_viewing_customers_spec.rb
+++ spec/features/admin_viewing_customers_spec.rb
@@ -1,16 +1,16 @@
 require 'rails_helper'
 
 feature 'Admin viewing customers' do
-	scenario 'with pagination' do
+  scenario 'with pagination' do
     company = Fabricate(:company)
     Fabricate.times(100, :customer, company: company)
 
-		visit customers_path
+    visit customers_path
 
     expect(page).to have_xpath(".//tbody//tr", :count => 50)
-	end
+  end
 
-	scenario 'filtering by company' do
+  scenario 'filtering by company' do
     company_1 = Fabricate(:company)
     Fabricate.times(5, :customer, company: company_1)
 
@@ -20,5 +20,5 @@ feature 'Admin viewing customers' do
     visit customers_path company_id: company_1.id
 
     expect(page).to have_xpath(".//tbody//tr", :count => 5)
-	end
+  end
 end
